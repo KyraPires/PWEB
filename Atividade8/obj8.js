821a10b50facfd0b33dfb9671a332109e41742dd
KyraPires <pires.vro@gmail.com>
2021-04-14 21:53:59 -0300

    Atividade 8

---
+++ Atividade8/obj8.js
@@ -0,0 +1,17 @@
+//forma 1
+
+var aluno1 = {
+    nome: "Leticia",
+    RA: "001234"
+}
+
+//forma 2
+var aluno2 = {};
+var nome = 'nome';
+aluno2[nome] = 'Leticia';
+console.log(objeto.nome);
+
+//forma 3
+var aluno3 = {};
+aluno3['nome'] = 'Silvia';
+console.log(aluno3.nome)
