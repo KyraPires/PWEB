0efa5f2b8aa6e4f1f4ff70ca63ea4dd472c890e5
KyraPires <pires.vro@gmail.com>
2021-04-14 21:54:12 -0300

    Atividade9

---
+++ Atividade9/obj9.js
@@ -0,0 +1,10 @@
+function retangulo(base, altura) {
+    this.base = base;
+    this.altura = altura;            
+}
+
+var objRetangulo = new retangulo(5, 3);
+objRetangulo.area = base * altura;
+
+alert(objRetangulo.base + " * " + ObjRetangulo.altura + ' área: ' + ObjRetangulo.area);
+
